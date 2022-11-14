e2e980a1883a76c312fc67b373f459a9fbc0c9cd
mszab0310 <mszabi0310@gmail.com>
2022-11-14 08:56:07 +0200

    Add new react project

---
+++ src/pages/StudentsListView.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+function StudentsListView() {
+  const [students, setStudents] = useState([]);
+  return <div>StudentsListView</div>;
+}
+
+export default StudentsListView;
